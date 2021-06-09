e8cd58452ae09f1a1b02e4b06a7baef25b180c34
Justin Parrondo <justinparrondo@gmail.com>
2021-06-08 23:23:52 -0400

    Finish production code for ex24

---
+++ src/main/java/org/cop3330/ex24/AnagramChecker.java
@@ -0,0 +1,26 @@
+package org.cop3330.ex24;
+
+import java.util.HashMap;
+
+public class AnagramChecker {
+    public boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length())
+            return false;
+
+        HashMap<Character, Integer> frequency1 = new HashMap<>();
+        HashMap<Character, Integer> frequency2 = new HashMap<>();
+
+        for (int i = 0; i < str1.length(); i++) {
+            char nextChar1 = str1.charAt(i);
+            char nextChar2 = str1.charAt(i);
+
+            int prevVal1 = frequency1.getOrDefault(nextChar1, 0);
+            int prevVal2 = frequency2.getOrDefault(nextChar2, 0);
+
+            frequency1.put(nextChar1, prevVal1 + 1);
+            frequency2.put(nextChar2, prevVal2 + 1);
+        }
+
+        return frequency1.equals(frequency2);
+    }
+}
--- src/main/java/org/cop3330/ex24/App.java
+++ src/main/java/org/cop3330/ex24/App.java
@@ -1,7 +1,20 @@
 package org.cop3330.ex24;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        AnagramChecker checker = new AnagramChecker();
 
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:");
+
+        System.out.print("Enter the first string: ");
+        String str1 = in.nextLine();
+        System.out.print("Enter the second string: ");
+        String str2 = in.nextLine();
+
+        String answer = checker.isAnagram(str1, str2) ? "anagrams" : "not anagrams";
+        System.out.printf("\"%s\" and \"%s\" are %s.\n", str1, str2, answer);
     }
 }
