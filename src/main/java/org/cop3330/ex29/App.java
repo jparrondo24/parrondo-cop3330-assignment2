2bdc0be8c0cb5d4ec137d57f998b66aa5af98b31
Justin Parrondo <justinparrondo@gmail.com>
2021-06-12 21:42:07 -0400

    Finish production code for ex29

--- src/main/java/org/cop3330/ex29/App.java
+++ src/main/java/org/cop3330/ex29/App.java
@@ -1,7 +1,20 @@
 package org.cop3330.ex29;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        RateValidator rv = new RateValidator();
 
+        do {
+            System.out.print("What is the rate of return? ");
+            String rateOfReturn = in.nextLine();
+            rv.setRate(rateOfReturn);
+            if (!rv.isValid())
+                System.out.println("Sorry. That's not a valid input.");
+        } while (!rv.isValid());
+
+        System.out.println(rv.generateYearsString());
     }
 }
---
+++ src/main/java/org/cop3330/ex29/RateValidator.java
@@ -0,0 +1,34 @@
+package org.cop3330.ex29;
+
+public class RateValidator {
+    private String rateOfReturn;
+    private double numericRate;
+
+    public void setRate(String rateOfReturn) {
+        this.rateOfReturn = rateOfReturn;
+    }
+    public boolean isValid() {
+        double rate;
+
+        try {
+            rate = Double.parseDouble(rateOfReturn);
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (rate == 0)
+            return false;
+
+        numericRate = rate;
+        return true;
+    }
+
+    private int calculateYears() {
+        return (int) Math.ceil(72 / numericRate);
+    }
+
+    public String generateYearsString() {
+        int years = calculateYears();
+        return String.format("It will take %d years to double your initial investment.", years);
+    }
+}
