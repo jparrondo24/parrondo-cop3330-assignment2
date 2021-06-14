cae42e113c91908ddf5feea0cf9e48119164f11b
Justin Parrondo <justinparrondo@gmail.com>
2021-06-13 23:23:10 -0400

    Finish test code for ex38

--- src/test/java/org/cop3330/ex38/EvenFilterTest.java
+++ src/test/java/org/cop3330/ex38/EvenFilterTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EvenFilterTest {
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+    EvenFilter ef = new EvenFilter(arr);
 
     @Test
     void filterEvenNumbers() {
+        int[] evens = {2, 4, 6, 8};
+        assertArrayEquals(ef.filterEvenNumbers(arr), evens);
     }
 
     @Test
     void testToString() {
+        assertEquals("The even numbers are 2 4 6 8.", ef.toString());
     }
 }
