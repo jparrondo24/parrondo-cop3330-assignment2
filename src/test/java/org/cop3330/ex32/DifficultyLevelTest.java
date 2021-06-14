f96df3b5bd05d10cc209cb3ea970dbb0e4958fa1
Justin Parrondo <justinparrondo@gmail.com>
2021-06-13 22:28:13 -0400

    Finish test code for ex34

---
+++ src/test/java/org/cop3330/ex32/DifficultyLevelTest.java
@@ -0,0 +1,16 @@
+package org.cop3330.ex32;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DifficultyLevelTest {
+    DifficultyLevel dl = new DifficultyLevel(1, 10);
+
+    @Test
+    void generateRandomNumber() {
+        // Yes.
+        // This is a unit test of a random option
+        // Selector.
+    }
+}
---
+++ src/test/java/org/cop3330/ex34/RemoveListTest.java
@@ -0,0 +1,16 @@
+package org.cop3330.ex34;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveListTest {
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
