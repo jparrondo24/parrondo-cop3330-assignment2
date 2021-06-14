8362a7594f319730ab48d92d59bec9ada9eace3c
Justin Parrondo <justinparrondo@gmail.com>
2021-06-13 22:35:03 -0400

    Finish test code for ex34

--- src/test/java/org/cop3330/ex34/RemoveListTest.java
+++ src/test/java/org/cop3330/ex34/RemoveListTest.java
@@ -5,12 +5,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RemoveListTest {
-
+    String[] employees = {
+            "John Smith",
+            "Jackie Jackson",
+            "Chris Jones",
+            "Amanda Cullen",
+            "Jeremy Goodwin"
+    };
+    RemoveList list = new RemoveList(employees);
     @Test
-    void remove() {
-    }
+    void testToStringRemovingAndToString() {
+        assertEquals(list.toString(),
+        "There are 5 employees:\n" +
+                "John Smith\n" +
+                "Jackie Jackson\n" +
+                "Chris Jones\n" +
+                "Amanda Cullen\n" +
+                "Jeremy Goodwin");
 
-    @Test
-    void testToString() {
+        list.remove("Chris Jones");
+        assertEquals(list.toString(),
+        "There are 4 employees:\n" +
+                "John Smith\n" +
+                "Jackie Jackson\n" +
+                "Amanda Cullen\n" +
+                "Jeremy Goodwin");
     }
 }
